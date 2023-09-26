2cffbef7f3bfd358b793776c043233be360912eb
Vladislav Drabovich <vladislav.drabovich@innowise-group.com>
2023-09-26 14:45:12 +0200

    fixed

--- main.go
+++ main.go
@@ -21,21 +21,21 @@ var (
 func downloadFile(inputUrl, filename string) error {
 	_, err := url.Parse(inputUrl)
 	if err != nil {
-		return ErrInvalidURL
+		return fmt.Errorf("invalid URL: %w: %w", ErrInvalidURL, err)
 	}
 
 	client := &http.Client{Timeout: 5 * time.Second}
 
 	resp, err := client.Get(inputUrl)
 	if err != nil {
-		return ErrConnectionFailed
+		return fmt.Errorf("connection error: &w: &w", ErrConnectionFailed, err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
 		if resp.StatusCode == http.StatusNotFound {
 			return ErrFileNotFound
 		}
-		return ErrDownloadFailed
+		return fmt.Errorf("file upload failed: %w: %w", ErrDownloadFailed, resp.StatusCode)
 	}
 
 	file, err := os.Create(filename)
@@ -51,14 +51,14 @@ func downloadFile(inputUrl, filename string) error {
 
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
-		return fmt.Errorf("failed to copy file %w", ErrDownloadFailed)
+		return fmt.Errorf("failed to copy file: %w: %w", ErrDownloadFailed, err)
 	}
 	return nil
 }
 
 func main() {
-	inputUrl := flag.String("url", "", "URL для загрузки файла")
-	output := flag.String("output", "", "Имя сохраненного файла")
+	inputUrl := flag.String("url", "", "URL for downloading file")
+	output := flag.String("output", "", "filename")
 
 	flag.Parse()
 
